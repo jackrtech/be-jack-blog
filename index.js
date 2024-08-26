3032635d16554efaaa53f6e2e5e301d1415698e6
Jack Rowsell <jack.rowsell.exmouth@gmail.com>
2024-08-26 20:55:14 +0100

    Setup for express server, created first endpoint

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json())
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
+app.get('/', async (req, res) => {
+    try {
+        const results = await pool.query('SELECT * FROM posts');
+        console.Consolelog('response test')
+        res.json(results.rows);
+    } catch (error) {
+        console.error(error.message);
+        console.log('error message')
+        res.status(500).send('Server error');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
