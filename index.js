8f861ae32a9233452cf09facb6c52a337c7410a9
Jack Rowsell <jack.rowsell.exmouth@gmail.com>
2024-08-28 20:08:43 +0100

    Endpoints defined

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const { resetDatabase, pool } = require('./db');
+const fs = require('fs')
 
 const app = express();
 app.use(express.json())
 
 
+app.get('/endpoints', (req, res) => {
+    fs.readFile('endpoints.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err.message);
+            return;
+        }
+        res.send(data);
+    });
+});
+
+
 app.get('/', async (req, res) => {
     try {
-       const results = await pool.query('SELECT * FROM posts');
+        const results = await pool.query('SELECT * FROM posts');
+        console.log(results.rows[1])
         res.json('response');
     } catch (error) {
         console.error(error.message);
@@ -18,7 +31,7 @@ app.get('/', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => { 
+app.listen(PORT, async () => {
     console.log(`Server started on port ${PORT} // http://localhost:${PORT}`)
     await resetDatabase()
 });
